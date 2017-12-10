12abc8711d2c43a1aca034e4583487146cbf490a
Allan Targino <altargin@microsoft.com>
2017-12-09 22:25:31 -0200

    comparing with GaussianNB

--- src/main.py
+++ src/main.py
@@ -3,16 +3,19 @@ from hyperPipes import HyperPipes
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import GaussianNB
 
-data = datasets.load_iris()
-X, y = data.data, data.target
-
+dataset = datasets.load_breast_cancer()
+X, y = dataset.data, dataset.target
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.33, random_state=42)
 
-classifier = HyperPipes()
-y_predicted = classifier.fit(X_train, y_train).predict(X_test)
-
+hp = HyperPipes()
+y_predicted = hp.fit(X_train, y_train).predict(X_test)
 accuracy = accuracy_score(y_test, y_predicted)
+print 'HyperPipes:\t' + str(accuracy)
 
-print accuracy
+classifier = GaussianNB()
+y_predicted = classifier.fit(X_train, y_train).predict(X_test)
+accuracy = accuracy_score(y_test, y_predicted)
+print 'GaussianNB:\t' + str(accuracy)
